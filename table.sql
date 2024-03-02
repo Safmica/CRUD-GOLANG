41002bc4148fa6b50487788722794a5db28698c8
Safmica <safmikasa@gmail.com>
2024-03-03 02:11:23 +0700

    Perbaikan isi table mahasiswa

--- table.sql
+++ table.sql
@@ -2,6 +2,9 @@ USE gdsc;
 
 CREATE TABLE mahasiswa (
 	nim INT PRIMARY KEY NOT NULL,
-	nama VARCHAR(50) NOT NULL
+	nama VARCHAR(50) NOT NULL,
+	jurusan VARCHAR(50) NOT NULL,
+	semester INT NOT NULL,
+	nomor_hp VARCHAR(50) NOT NULL
 )
 
