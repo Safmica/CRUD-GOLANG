2211bb8bd012e499c66143ecf0fe6c511ce72fa5
Safmica <safmikasa@gmail.com>
2024-03-03 11:48:56 +0700

    Memperbaiki fungsi yang salah pada database.go

--- database_connection/database.go
+++ database_connection/database.go
@@ -1,21 +1,22 @@
 package database_connection
 
-import(
-	_ "github.com/go-sql-driver/mysql"
-	"database/sql"
-	"time"
+import (
+    _ "github.com/go-sql-driver/mysql"
+    "database/sql"
+    "time"
 )
 
 func GetConnection() *sql.DB {
-	db,err := sql.Open("mysql", "root:@tcp(localhost:3306)/gdsc")
-	if err != nil{
-		panic(err)
-	}
+    db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/gdsc")
+    if err != nil {
+        panic(err)
+    }
 
-	db.SetMaxIdleConns(10)
-	db.SetMaxOpenConns(100)
-	db.SetMaxIdleTime(5 * time.Minute)
-	db.SetMaxLifeTime(60 * time.Minute)
+    db.SetMaxIdleConns(10)
+    db.SetMaxOpenConns(100)
+    db.SetConnMaxIdleTime(5 * time.Minute)
+    db.SetConnMaxLifetime(60 * time.Minute)
 
-	return db
+    return db
 }
+
